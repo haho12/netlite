3e1c92250f9ffc796de4127976ab6b75b890707a
Hanno Homann <hanno.homann@hs-hannover.de>
2025-09-12 18:41:01 +0200

    finalize BatchNorm()

--- src/netlite/layers.py
+++ src/netlite/layers.py
@@ -242,25 +242,28 @@ class GlobalAvgPoolingLayer(Layer):
         n, c = grad_backward.shape    
         return grad_backward.reshape(n, 1, 1, c) * self.mask
 
-# todo...
-# https://chatgpt.com/c/68c07dd3-62fc-8332-8928-fea151e2183f
 class BatchNorm(Layer):
     def forward(self, X):
         training_mode = (X.shape[0] > 1)
-        #self.X = X
-        self.eps = 1e-7
+        self.beta = 0.9
+        self.eps  = 1e-7
         
         if training_mode:
             # compute batch statistics
-            self.batch_mean = np.mean(X, axis=0, keepdims=True)
-            self.batch_var = np.var(X, axis=0, keepdims=True)
+            batch_mean = np.mean(X, axis=0, keepdims=True)
+            batch_var = np.var(X, axis=0, keepdims=True)
+
+            # soft updates
+            if not hasattr(self, 'running_mean'):
+                # initialization
+                self.running_mean = batch_mean
+                self.running_var  = batch_var
+            else:
+                self.running_mean = self.beta*self.running_mean - (1-self.beta)*batch_mean
+                self.running_var  = self.beta*self.running_var  - (1-self.beta)*batch_var
 
             # normalize
-            x_out = (X - self.batch_mean) / np.sqrt(self.batch_var + self.eps)
-            
-            # todo: soft updates
-            self.running_mean = self.batch_mean
-            self.running_var  = self.batch_var
+            x_out = (X - self.running_mean) / np.sqrt(self.running_var + self.eps)
         else:
             # Use running averages at inference
             x_out = (X - self.running_mean) / np.sqrt(self.running_var + self.eps)
@@ -268,8 +271,8 @@ class BatchNorm(Layer):
         return x_out
 
     def backward(self, grad_backward):
-        # todo: double-check this
         # gradient wrt input
+        assert hasattr(self, 'running_var'), 'BatchNorm forward must be called first.'
         dx = grad_backward / np.sqrt(self.batch_var + self.eps)
 
         return dx
