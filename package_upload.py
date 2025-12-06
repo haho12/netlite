933ef3f0286b4d8ccb8f4d44b43be7fc4e203c7c
Hanno Homann <hanno.homann@hs-hannover.de>
2025-12-06 17:40:42 +0100

    convert package scripts to python

--- package_upload.py
+++ package_upload.py
@@ -1,57 +1,79 @@
+#!/usr/bin/env python
 import tomllib
 import requests
 from packaging.version import Version
 import sys
 import subprocess
 
+from package_run_tests import run_tests
+from package_build import build_package
+
 PACKAGE_NAME = "netlite"
 
-# 0. Check code is fully committed in git
-status = subprocess.run(
-    ["git", "status", "--porcelain"],
-    capture_output=True,
-    text=True,
-    check=True
-).stdout.strip()
+# Steps:
+# - run pytest
+# - build (with new release number)
+# - push to github (with release tag)
+# - upload to pipy
 
-if status:
-    print("Error: Working tree is dirty. Commit or stash your changes first.")
-    print(status)
-    sys.exit(1)
-    
-# 1. Read local version from pyproject.toml
-with open("pyproject.toml", "rb") as f:
-    pyproject = tomllib.load(f)
+def git_tag_and_push():
+    # Check code is fully committed in git
+    status = subprocess.run(
+        ["git", "status", "--porcelain"],
+        capture_output=True,
+        text=True,
+        check=True
+    ).stdout.strip()
 
-local_version = pyproject["project"]["version"]
+    if status:
+        print("Error: Working tree is dirty. Commit or stash your changes first.")
+        print(status)
+        return False
 
-# 2. Get latest version from PyPI
-url = f"https://pypi.org/pypi/{PACKAGE_NAME}/json"
-pypi_version = requests.get(url, timeout=10).json()["info"]["version"]
+    subprocess.run(['git', 'push'])                # push commits
+        
+    # 1. Read local version from pyproject.toml
+    with open("pyproject.toml", "rb") as f:
+        pyproject = tomllib.load(f)
 
-# 3. Get latest git tag
-git_tag = subprocess.run(
-    ["git", "describe", "--tags", "--abbrev=0"],
-    capture_output=True,
-    text=True,
-    check=True
-).stdout.strip()
-# strip leading "v" (v1.2.3 → 1.2.3)
-git_version = git_tag.lstrip("v")
+    local_version = pyproject["project"]["version"]
 
-# 4. Compare
-print("version in pyproject.toml: ", local_version)
-print("version in git tag:        ", git_version)
-print("version on pypi.org:       ", pypi_version)
-if Version(git_version) < Version(local_version):
-    git_tag_new = 'v' + local_version
-    print(f"Info: Updating git tag to '{git_tag_new}'")
-    subprocess.run(['git', 'tag', git_tag_new])
+    # 2. Get latest version from PyPI
+    #url = f"https://pypi.org/pypi/{PACKAGE_NAME}/json"
+    #pypi_version = requests.get(url, timeout=10).json()["info"]["version"]
 
+    # 3. Get latest git tag
+    git_tag = subprocess.run(
+        ["git", "describe", "--tags", "--abbrev=0"],
+        capture_output=True,
+        text=True,
+        check=True
+    ).stdout.strip()
+    # strip leading "v" (v1.2.3 → 1.2.3)
+    git_version = git_tag.lstrip("v")
+
+    # 4. Compare
+    print("version in pyproject.toml: ", local_version)
+    print("version in git tag:        ", git_version)
+    #print("version on pypi.org:       ", pypi_version)
+    if Version(git_version) < Version(local_version):
+        git_tag_new = 'v' + local_version
+        print(f"Info: Updating git tag to '{git_tag_new}'")
+        subprocess.run(['git', 'tag', git_tag_new])
+        subprocess.run(['git', 'push', git_tag_new])   # push new tag
+
+        sys.exit()
+
+success = run_tests()
+if not success:
     sys.exit()
 
-#subprocess.run(['rm', '-rf', 'dist'])
-#subprocess.run(['python', '-m', 'build'])
+success = build_package(require_new_version=True)
+if not success:
+    sys.exit()
 
+success = git_tag_and_push()
+if not success:
+    sys.exit()
 
-#twine upload dist/*
+#subprocess.run(['twine', 'upload', 'dist/*'])
